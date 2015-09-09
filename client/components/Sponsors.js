cc81028f00243e7d4228fa1562473dc414eb5f77
Adhithya Padmanabhan <adhithyabalaji1997@gmail.com>
2015-09-09 22:58:28 +0530

    Feat(Sponsors2015):Implemented Sponsors page

--- client/components/Sponsors.js
+++ client/components/Sponsors.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 
-class Sponsors {
+import '../styles/sponsors.css';
+
+class Sponsors extends Component {
   render() {
     return (
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      <div className="sponsors-page">
+        <div className="sponsors-title"> SPONSORS 2015 </div>
+        <SponsorDetails />
       </div>
     );
   }
+}
+
+class SponsorDetails  {
+    static propTypes = {
+      width: PropTypes.number.isRequired,
+      imgsrc: PropTypes.string.isRequired
+    }
+
+    render()  {
+      return (
+        <div>
+        </div>
+      );
+    }
 }
 
 export default Sponsors;
